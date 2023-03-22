804473f92e2915b30d2e9dd6738d62ce0a5a757e
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2023-03-22 16:54:41 +0530

    properly change the bundlename

--- android/app/src/main/java/com/aathichoodi/MainActivity.java
+++ android/app/src/main/java/com/aathichoodi/MainActivity.java
@@ -1,4 +1,4 @@
-package com.aathichoodi;
+package xyz.nibaji.aathichoodi;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/aathichoodi/MainApplication.java
+++ android/app/src/main/java/com/aathichoodi/MainApplication.java
@@ -1,4 +1,4 @@
-package com.aathichoodi;
+package xyz.nibaji.aathichoodi;
 
 import android.app.Application;
 import com.facebook.react.PackageList;

38dcc8ff0190080a31cbbaab475ec99ee4204f1f
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2023-03-30 22:34:48 +0530

    theme: prevent white flicker

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {View} from 'react-native';
 import {Provider} from 'react-redux';
 
 import StackedNavigation from './src/Navigation/stackedNavigation';
@@ -6,11 +7,15 @@ import InfoModal from './src/Components/InfoModal';
 
 import store from './src/Redux/store';
 
+import AppColor from './src/Theme/colors';
+
 export default function App() {
   return (
     <Provider store={store}>
-      <StackedNavigation />
-      <InfoModal />
+      <View style={{flex: 1, backgroundColor: AppColor.accent}}>
+        <StackedNavigation />
+        <InfoModal />
+      </View>
     </Provider>
   );
 }
